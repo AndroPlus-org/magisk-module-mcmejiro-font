595e263bc3d97a21fd2e2c16cce41495b6e3d630
AndroPlus <hosigumayuugi@gmail.com>
2022-02-23 10:43:03 +0900

    first commit

---
+++ post-fs-data.sh
@@ -0,0 +1,102 @@
+#!/system/bin/sh
+# Do NOT assume where your module will be located.
+# ALWAYS use $MODDIR if you need to know where this script
+# and module is placed.
+# This will make sure your module will still work
+# if Magisk change its mount point in the future
+MODDIR=${0%/*}
+# This script will be executed in post-fs-data mode
+
+#Copy original fonts.xml to the MODDIR to overwrite dummy file
+cp /system/etc/fonts.xml $MODDIR/system/etc
+
+#Function to remove original ja
+remove_ja() {
+  sed -i -e '/<family lang="ja"/,/<\/family>/d' $1
+}
+
+#Function to add ja above zh-Hans
+add_ja() {
+	if [ $API -ge 31 ] ; then
+		#Android 12 and later
+		sed -i 's@<family lang="zh-Hans">@<family lang="ja">\n        <font weight="100" style="normal" postScriptName="NotoSansCJKjp-Regular">McMejiro-Thin.ttf</font>\n        <font weight="300" style="normal" postScriptName="NotoSansCJKjp-Regular">McMejiro-Light.ttf</font>\n        <font weight="400" style="normal" postScriptName="NotoSansCJKjp-Regular">McMejiro-Regular.ttf</font>\n        <font weight="600" style="normal" postScriptName="NotoSansCJKjp-Regular">McMejiro-Semibold.ttf</font>\n        <font weight="700" style="normal" postScriptName="NotoSansCJKjp-Regular">McMejiro-Bold.ttf</font>\n        <font weight="800" style="normal" postScriptName="NotoSansCJKjp-Regular">McMejiro-Extrabold.ttf</font>\n        <font weight="100" style="normal" postScriptName="NotoSansCJKjp-Regular" fallbackFor="serif">McMejiro-Thin.ttf</font>\n        <font weight="300" style="normal" postScriptName="NotoSansCJKjp-Regular" fallbackFor="serif">McMejiro-Light.ttf</font>\n        <font weight="400" style="normal" postScriptName="NotoSansCJKjp-Regular" fallbackFor="serif">McMejiro-Regular.ttf</font>\n        <font weight="600" style="normal" postScriptName="NotoSansCJKjp-Regular" fallbackFor="serif">McMejiro-Semibold.ttf</font>\n        <font weight="700" style="normal" postScriptName="NotoSansCJKjp-Regular" fallbackFor="serif">McMejiro-Bold.ttf</font>\n        <font weight="800" style="normal" postScriptName="NotoSansCJKjp-Regular" fallbackFor="serif">McMejiro-Extrabold.ttf</font>\n    </family>\n    <family lang="zh-Hans">@g' $1
+	else
+		sed -i 's@<family lang="zh-Hans">@<family lang="ja">\n        <font weight="100" style="normal">McMejiro-Thin.ttf</font>\n        <font weight="300" style="normal">McMejiro-Light.ttf</font>\n        <font weight="400" style="normal">McMejiro-Regular.ttf</font>\n        <font weight="600" style="normal">McMejiro-Semibold.ttf</font>\n        <font weight="700" style="normal">McMejiro-Bold.ttf</font>\n        <font weight="800" style="normal">McMejiro-Extrabold.ttf</font>\n        <font weight="100" style="normal" fallbackFor="serif">McMejiro-Thin.ttf</font>\n        <font weight="300" style="normal" fallbackFor="serif">McMejiro-Light.ttf</font>\n        <font weight="400" style="normal" fallbackFor="serif">McMejiro-Regular.ttf</font>\n        <font weight="600" style="normal" fallbackFor="serif">McMejiro-Semibold.ttf</font>\n        <font weight="700" style="normal" fallbackFor="serif">McMejiro-Bold.ttf</font>\n        <font weight="800" style="normal" fallbackFor="serif">McMejiro-Extrabold.ttf</font>\n    </family>\n    <family lang="zh-Hans">@g' $1
+	fi
+}
+
+#Function to replace Google Sans
+replace_gsans() {
+	sed -i 's@GoogleSans-Italic.ttf@GoogleSans-Regular.ttf\n      <axis tag="ital" stylevalue="1" />@g' $1
+}
+
+#Change fonts.xml file
+remove_ja $MODDIR/system/etc/fonts.xml
+add_ja $MODDIR/system/etc/fonts.xml
+
+gsans=/system/product/etc/fonts_customization.xml
+if [ -e $gsans ]; then
+	cp $gsans $MODDIR$gsans
+	replace_gsans $MODDIR$gsans
+fi
+
+#Goodbye, SomcUDGothic
+sed -i 's@SomcUDGothic-Light.ttf@null.ttf@g' $MODDIR/system/etc/fonts.xml
+sed -i 's@SomcUDGothic-Regular.ttf@null.ttf@g' $MODDIR/system/etc/fonts.xml
+
+#Goodbye, OnePlus Font
+sed -i 's@OpFont-@Roboto-@g' $MODDIR/system/etc/fonts.xml
+sed -i 's@NotoSerif-@Roboto-@g' $MODDIR/system/etc/fonts.xml
+
+#Goodbye, OPLUS Font
+sed -i 's@SysFont-Regular@Roboto-Regular@g' $MODDIR/system/etc/fonts.xml
+sed -i 's@SysSans-En-Regular@Roboto-Regular@g' $MODDIR/system/etc/fonts.xml
+
+#Goodbye, Xiaomi Font
+/system/bin/sed -i -z 's@<family name="sans-serif">\n    <!-- # MIUI Edit Start -->.*<!-- # MIUI Edit END -->@<family name="sans-serif">@' $MODDIR/system/etc/fonts.xml
+sed -i 's@MiSansVF.ttf@Roboto-Regular.ttf@g' $MODDIR/system/etc/fonts.xml
+
+#Goodbye, Sansita Font
+sed -i 's@Sansita-@Roboto-@g' $MODDIR/system/etc/fonts.xml
+
+#Copy fonts_slate.xml for OnePlus
+opslate=fonts_slate.xml
+if [ -e /system/etc/$opslate ]; then
+    cp /system/etc/$opslate $MODDIR/system/etc
+	
+	#Change fonts_slate.xml file
+	remove_ja $MODDIR/system/etc/$opslate
+	add_ja $MODDIR/system/etc/$opslate
+
+	sed -i 's@SlateForOnePlus-Thin.ttf@McMejiro-Light.ttf@g' $MODDIR/system/etc/$opslate
+	sed -i 's@SlateForOnePlus-Light.ttf@McMejiro-Light.ttf@g' $MODDIR/system/etc/$opslate
+	sed -i 's@SlateForOnePlus-Book.ttf@McMejiro-Regular.ttf@g' $MODDIR/system/etc/$opslate
+	sed -i 's@SlateForOnePlus-Regular.ttf@McMejiro-Regular.ttf@g' $MODDIR/system/etc/$opslate
+	sed -i 's@SlateForOnePlus-Medium.ttf@McMejiro-Semibold.ttf@g' $MODDIR/system/etc/$opslate
+	sed -i 's@SlateForOnePlus-Bold.ttf@McMejiro-Bold.ttf@g' $MODDIR/system/etc/$opslate
+	sed -i 's@SlateForOnePlus-Black.ttf@McMejiro-Extrabold.ttf@g' $MODDIR/system/etc/$opslate
+fi
+
+#Copy fonts_base.xml for OnePlus OxygenOS 11
+oos11=fonts_base.xml
+if [ -e /system/etc/$oos11 ]; then
+    cp /system/etc/$oos11 $MODDIR/system/etc
+	
+	#Change fonts_slate.xml file
+	remove_ja $MODDIR/system/etc/$oos11
+	add_ja $MODDIR/system/etc/$oos11
+
+	sed -i 's@NotoSerif-@Roboto-@g' $MODDIR/system/etc/$oos11
+fi
+
+#Copy fonts_base.xml for OnePlus OxygenOS 12+
+oos12=fonts_base.xml
+if [ -e /system/system_ext/etc/$oos12 ]; then
+    cp /system/system_ext/etc/$oos12 $MODDIR/system/system_ext/etc
+	
+	#Change fonts_slate.xml file
+	remove_ja $MODDIR/system/system_ext/etc/$oos12
+	add_ja $MODDIR/system/system_ext/etc/$oos12
+
+	sed -i 's@NotoSerif-@Roboto-@g' $MODDIR/system/system_ext/etc/$oos12
+fi
